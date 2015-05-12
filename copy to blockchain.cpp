3336f3936bd9d1340649be9afa3e3a50ffec498e
gilgvirts <gilgvirts@users.noreply.github.com>
2015-05-12 16:35:29 +0300

    Update copy to blockchain.cpp

--- copy to blockchain.cpp
+++ copy to blockchain.cpp
@@ -1,23 +1,4 @@
-int was_added(int block_num)
+int chain_size()
 {
-	Node* block;
-	try
-	{
-		if (!gIdHandler->idExists(block_num))
-			{
-				return -2;
-			}
-		block = find_pre_hashed_block(block_num);
-
-		if (block == NULL)
-		{
-			return 1;
-		}
-	}
-	catch (exception& e)
-	{
-		// if "OTHER ERRORS" RETURN -1
-		return -1;
-	}
-	return 0;
+	return gNumOfBlk;
 }
